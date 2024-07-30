f9fac537ac8af1da0d124c6b5afc1e1b70251bc0
Harsh Chafle <157059028+Harshchafle@users.noreply.github.com>
2024-07-31 00:56:30 +0530

    Update StringPermutation.java

--- Backtracking/StringPermutation.java
+++ Backtracking/StringPermutation.java
@@ -1,10 +1,12 @@
 public class StringPermutation {
     // Method to find Permutations of a String
     public static void printPermutation(String inStr, String permStr, int idx){
+        
         if(inStr.length() == 0){
             System.out.println(permStr);
             return;
         }
+        
         for(int i=0; i<inStr.length(); i++){
             char currChar = inStr.charAt(i);
             String newStr = inStr.substring(0,i)+inStr.substring(i+1);
