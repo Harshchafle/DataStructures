3a73dbf57a94f1b98f2ed2e9fb0b8c4d8ec46a27
Harshchafle <chafleharsh@gmail.com>
2025-08-20 22:22:09 +0530

    added headless

--- scripts/leetcode_sync.py
+++ scripts/leetcode_sync.py
@@ -41,7 +41,7 @@ class LeetCodeSync:
         print("🚀 Setting up Chrome driver...")
         
         chrome_options = Options()
-        # chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument('--disable-gpu')
